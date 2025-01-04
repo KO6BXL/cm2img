d9c6fee78905cb9a0c4beb948966256aa09c5982
KO6BXL <coggeshalladam@gmail.com>
2025-01-04 00:12:10 -0800

    Added fine mode

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ import (
 )
 
 func main() {
-	var imgFile = os.Args[1]
+	var mode = os.Args[1]
+	var imgFile = os.Args[2]
 
 	img, err := os.Open(imgFile)
 
@@ -47,6 +48,27 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	if mode == "normal" {
+		out, err := build.FastCompile([]block.Collection{normMode(Image)})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(out)
+	} else if mode == "fine" {
+		out, err := build.Compile([]block.Collection{fineMode(Image)})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(out)
+	}
+
+}
+
+func normMode(Image image.Image) block.Collection {
 
 	MaxX := Image.Bounds().Max.X
 	MaxY := Image.Bounds().Max.Y
@@ -59,10 +81,6 @@ func main() {
 		for iy := 0; iy < MaxY; iy++ {
 			red, green, blue, alpha := Image.At(ix, iy).RGBA()
 
-			if err != nil {
-				log.Fatal(err)
-			}
-
 			var color2 block.Color
 
 			color2.R = uint8(red / bitshift / bitshift)
@@ -70,7 +88,7 @@ func main() {
 			color2.B = uint8(blue / bitshift / bitshift)
 			a := uint8(alpha)
 
-			if !(a < 15) {
+			if !(a < 45) {
 				thingieidk = collection.Append(block.TILE(color2, 2))
 				thingieidk.Offset.X = float32(ix)
 				thingieidk.Offset.Z = float32(iy)
@@ -78,14 +96,48 @@ func main() {
 
 		}
 	}
-	out, err := build.FastCompile([]block.Collection{collection})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(out)
+	return collection
 }
+
+func fineMode(Image image.Image) block.Collection {
+
+	MaxX := Image.Bounds().Max.X
+	MaxY := Image.Bounds().Max.Y
+
+	var collection block.Collection
+	var thingieidk *block.Base
+	var yCount float32 = 0
+	var yyCount int = 0
+	var bitshift uint32 = 1 << 4
+	for ix := 0; ix < MaxX; ix++ {
+		yCount = yCount + 0.0005
+		var z float32 = yCount
+
+		for iy := 0; iy < MaxY; iy++ {
+			red, green, blue, alpha := Image.At(ix, iy).RGBA()
+
+			var color2 block.Color
+
+			color2.R = uint8(red / bitshift / bitshift)
+			color2.G = uint8(green / bitshift / bitshift)
+			color2.B = uint8(blue / bitshift / bitshift)
+			a := uint8(alpha / bitshift / bitshift)
+			z = z - 0.0005
+
+			if !(a < 45) {
+				thingieidk = collection.Append(block.TILE(color2, 2))
+				thingieidk.Offset.X = float32(float32(ix) / 5)
+				thingieidk.Offset.Y = float32(z)
+				thingieidk.Offset.Z = float32(float32(iy) / 5)
+			}
+
+		}
+		yyCount = yyCount + 1
+
+	}
+	return collection
+}
+
 func getImage(img *os.File) (image.Image, error) {
 	if path.Ext(img.Name()) == ".jpeg" || path.Ext(img.Name()) == ".jpg" {
 		Image, err := jpeg.Decode(img)
