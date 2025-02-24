1f92db5f91cb02083bc48135a5123781cfd1018b
ko6bxl <coggeshalladam@gmail.com>
2025-02-24 14:06:17 -0800

    Add better arg parser for the cli

--- cli/main.go
+++ cli/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 
 	"github.com/ko6bxl/cm2img"
 )
 
 func main() {
-	mode := os.Args[1]
-	file := os.Args[2]
-	fmt.Println(cm2img.Gen(mode, file))
+	var mode string
+	flag.StringVar(&mode, "m", "fine", "Mode to generate with: either 'fine' or 'normal'")
+	var file string
+	flag.StringVar(&file, "i", "USER IS BEING VERY DUMB", "File to convert into save, REQUIRED TO FUNCTION!")
+	flag.Parse()
+
+	if file == "USER IS BEING VERY DUMB" {
+		fmt.Println("Input a file because file=" + file)
+	} else {
+		fmt.Println(cm2img.Gen(mode, file))
+	}
+
 }
